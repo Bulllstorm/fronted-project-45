58a71dc1cbddeba4497ec07666b1981902f14c1f
Ваше Имя <kolia4246868@gmail.com>
2025-02-05 18:39:28 +0300

    Add new games

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,43 +1,39 @@
 #!/usr/bin/env node
-import readLineSync from "readline-sync";
+import readLineSync from 'readline-sync';
+import chalk from 'chalk';
 
 console.log('Welcome to Brain Games!');
 
-//Запрашиваем имя
+// Запрашиваем имя
 const askName = () => {
-    const userName = readLineSync.question('What is your name?');
-    console.log(`Hello, ${userName}`);
-    return userName;
-}
-//Генерируем число
-const getRandomNumber = () => {
-      return Math.floor(Math.random() * 100);
+  const userName = readLineSync.question('What is your name?');
+  console.log(chalk.blue(`Hello, ${userName}`));
+  return userName;
 };
-//Определяем является ли число четным
-const isEven = (number) => {
-    return number % 2 === 0 ? 'yes' : 'no';
-}
-//Запрос ответа пользователя
+// Генерируем число
+const getRandomNumber = () => Math.floor(Math.random() * 100);
+// Определяем является ли число четным
+const isEven = (number) => (number % 2 === 0 ? 'yes' : 'no');
+// Запрос ответа пользователя
 const getUserAnswer = () => readLineSync.question('Your answer: ');
-
-
-//Логика игры
-const playGame = () => {
-    const rounds = 3;
-    for (let i = 0; i < rounds; i++) {
-        const number = getRandomNumber();
-        console.log(`Question: ${number}`);
-        const correctAnswer = isEven(number);
-        const userAnswer = getUserAnswer();
-        if (userAnswer === correctAnswer) {
-            console.log('Correct!');
-        } else {
-            console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-            console.log(`Lets try again ${userName}`);
-            return;
-        }
-    }
-    console.log('Congratulations! You won!');
-};
 const userName = askName();
+// Логика игры
+const playGame = () => {
+  const rounds = 3;
+  for (let i = 0; i < rounds; i + 1) {
+    const number = getRandomNumber();
+    console.log(`Question: ${number}`);
+    const correctAnswer = isEven(number);
+    const userAnswer = getUserAnswer();
+    if (userAnswer === correctAnswer) {
+      console.log('Correct!');
+    } else {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`Lets try again ${userName}`);
+      return;
+    }
+  }
+  console.log('Congratulations! You won!');
+};
+
 playGame();
